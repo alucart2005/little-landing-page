7da5a979e31a2208459d9024b6b5832251ea9110
Napoleon Anaya R <napoleonanaya@gmail.com>
2023-07-09 20:56:41 -0500

    feat: add preview image

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const toggle = document.querySelector('.toggle');
+const navigation = document.querySelector('.navigation');
 
 toggle.addEventListener('click', () => {
   toggle.classList.toggle('toggle--on');
+  navigation.classList.toggle('navigation--on');
 } );
